c6ea2e928ee95c28901802de4609a72306a19d6d
aibrusci <aibrusci@unix1.csc.calpoly.edu>
2019-04-08 13:28:21 -0700

    lab1

--- lab1.py
+++ lab1.py
@@ -1,15 +1,39 @@
 
-def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+def max_list_iter(int_list):
+    if int_list == None:
+        raise ValueError()
+    if len(int_list) > 0:
+        largest = int_list[0]
+        for i in int_list:
+            if i > largest:
+                largest = i
+        return largest
+    else:
+        return None
 
-def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
+def reverse_rec(int_list):
+    if int_list == None:
+        raise ValueError()
+    else:
+        if len(int_list) == 0:
+            return []
+        return [int_list[-1]] + reverse_rec(int_list[:-1])  
+         
 
-def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+def bin_search(target, low, high, int_list):
+    if int_list == None:
+        raise ValueError()
+    else:
+        mid = (high + low) // 2
+        if int_list[mid] == target:
+            return mid
+        else:
+            mid = (high + low) // 2
+            if target <= int_list[mid] and target >= int_list[low]:
+                high = mid 
+                return bin_search(target, low, high, int_list)
+            elif target >= int_list[mid] and target <= int_list[high]:
+                low = mid + 1
+                return bin_search(target, low, high, int_list)
+            else:
+                return None
