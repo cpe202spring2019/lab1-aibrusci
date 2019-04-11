9fa6561f442e07c578d790e4b624bcc08e6f08eb
Amelia Bruscia <aibrusci@unix1.csc.calpoly.edu>
2019-04-10 19:40:23 -0700

    lab1

--- lab1.py
+++ lab1.py
@@ -11,6 +11,7 @@ def max_list_iter(int_list):
     else:
         return None
 
+
 def reverse_rec(int_list):
     if int_list == None:
         raise ValueError()
@@ -18,19 +19,21 @@ def reverse_rec(int_list):
         if len(int_list) == 0:
             return []
         return [int_list[-1]] + reverse_rec(int_list[:-1])  
-         
+
 
 def bin_search(target, low, high, int_list):
     if int_list == None:
         raise ValueError()
     else:
+        if len(int_list) == 0:
+            return None
         mid = (high + low) // 2
         if int_list[mid] == target:
             return mid
         else:
             mid = (high + low) // 2
             if target <= int_list[mid] and target >= int_list[low]:
-                high = mid 
+                high = mid
                 return bin_search(target, low, high, int_list)
             elif target >= int_list[mid] and target <= int_list[high]:
                 low = mid + 1
